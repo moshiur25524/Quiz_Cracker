8b0837386f9465fa2ed84cdf87e1e1694d7be838
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-03-31 16:54:35 +0600

    4 card created in the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import HeroBanner from '../HeroBanner/HeroBanner';
+import { useLoaderData } from 'react-router-dom';
+import Quiz from '../Quiz/Quiz';
 
 const Home = () => {
+    const Allquiz = useLoaderData()
     return (
         <div>
-            <HeroBanner/>
-            <h1>Hello World</h1>
-            <h2 className="text-3xl font-bold">
-                Hello world!
-            </h2>
-            <button>Buy Now</button>
+            <HeroBanner />
+            <div className="h-screen grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
+                {Allquiz.data.map(quiz => <Quiz
+                    key={quiz.id}
+                    quiz={quiz}
+                />)}
+            </div>
+           
         </div>
     );
 };
---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Quiz = ({ quiz }) => {
+    const { id, name, logo, total } = quiz;
+    return (
+        <div>
+            <div className=" bg-white rounded-lg shadow-md overflow-hidden">
+                <img className="w-full h-48 object-cover" src={logo} alt={name} />
+                <div className="px-4 py-2">
+                    <h3 className="text-lg font-medium text-gray-800">{name}</h3>
+                    <Link to='/' className="mt-2 inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-all">
+                        Start Practice
+                    </Link>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Quiz;
